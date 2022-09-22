1fe21661b4bb6055e4f99c33773c8e3d7f1480e4
Martin Alberti <martinalberti123@gmail.com>
2022-09-22 17:33:12 -0300

    styles

--- hbs/Products.js
+++ hbs/Products.js
@@ -2,49 +2,49 @@ class Products {
 	productos = [
 		{
 		  id: 1,
-		  name: 'Escuadra',
+		  title: 'Escuadra',
 		  price: 323.45,
 		  thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Squadra_45.jpg'
 		},
 		{
 		  id: 2,
-		  name: 'Calculadora',
+		  title: 'Calculadora',
 		  price: 234.56,
 		  thumbnail: 'https://micalculadoracientifica.com/wp-content/uploads/2021/01/TI-Nspire-CX-Amazon.jpg'
 		},
 		{
 		  id: 3,
-		  name: 'Globo Terráqueo',
+		  title: 'Globo Terráqueo',
 		  price: 45.67,
 		  thumbnail: 'https://panamericana.vteximg.com.br/arquivos/ids/256800-600-690/globo-terraqueo-politico-40-cm-7701016736787.jpg?v=636381897120030000'
 		},
 		{
 		  id: 4,
-		  name: 'Paleta Pintura',
+		  title: 'Paleta Pintura',
 		  price: 456.78,
 		  thumbnail: 'https://www.botiga.com.uy/media/catalog/product/cache/1/image/600x600/0dc2d03fe217f8c83829496872af24a0/p/a/paleta_pintora_tempera_infantozzi_materiales.jpg'
 		},
 		{
 		  id: 5,
-		  name: 'Reloj',
+		  title: 'Reloj',
 		  price: 67.89,
 		  thumbnail: 'https://us.123rf.com/450wm/monticello/monticello1911/monticello191100379/135078958-reloj-de-pared-aislado-sobre-fondo-blanco-nueve-.jpg?ver=6'
 		},
 		{
 		  id: 6,
-		  name: 'Agenda',
+		  title: 'Agenda',
 		  price: 78.90,
 		  thumbnail: 'https://cloudfront-eu-central-1.images.arcpublishing.com/prisa/AGYRBXKZQH6C4KYQU6IGD2BDIE.jpg'
 		},
 		{
 		  id: 7,
-		  name: 'Escudo caballero templario',
+		  title: 'Escudo caballero templario',
 		  price: 456.78,
 		  thumbnail: 'https://www.tienda-medieval.com/blog/wp-content/uploads/2010/09/escudo_templario1.jpg'
 		},
 		{
 		  id: 8,
-		  name: 'Escorpión de juguete',
+		  title: 'Escorpión de juguete',
 		  price: 1000.87,
 		  thumbnail: 'https://sc04.alicdn.com/kf/H5794a667d8844b0592a7a76e8724842bt.jpg'
 		},
